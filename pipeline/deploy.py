3b88283bc921cf6857c0db730747577caa791cad
pasan1112 <irindukodithuwakku12@gmail.com>
2026-05-06 18:40:48 +0530

    clean

--- pipeline/deploy.py
+++
@@ -1,5 +0,0 @@
-import time
-
-def run_deploy():
-    print("Running deploy stage...")
-    time.sleep(5)
--- pipeline/test.py
+++
@@ -1,7 +0,0 @@
-import time
-
-def run_tests():
-    print("Running test stage...")
-    for _ in range(10):
-        for i in range(20_000_000):
-            pass
